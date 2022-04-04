7838c8a5f034492272a0f96b5a03fbe18716eb27
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-04 12:59:14 +0200

    Creating a Cart Context and a CartProvider component to wrap other components which need access to the context

---
+++ src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemFromCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
